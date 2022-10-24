e5be36e01a480780390509a395064b03965d90f1
Jean <jeanncavalcante@gmail.com>
2022-10-24 20:47:14 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App h-screen bg-gray-200">
+      <h1>Hello loja</h1>
+      <Button variant="contained">Hello World 123</Button>
+    </div>
+  );
+}
+
+export default App;
