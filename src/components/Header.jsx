8a93820e28fcff96f939968197821699e5ffb05a
Jean <jeanncavalcante@gmail.com>
2022-10-25 12:13:25 -0300

    add Theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { MdLocalGroceryStore, MdStoreMallDirectory } from "react-icons/md";
+import { BsFillSunFill, BsFillMoonStarsFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useThemeContext } from "../hooks/useThemeContext";
 
 const Header = () => {
+  const { theme, setTheme } = useThemeContext();
   return (
-    <header className="flex bg-gray-300 py-[20px] px-1 justify-around">
+    <header className="flex bg-gray-300 dark:bg-gray-800 dark:text-white py-[20px] px-1 justify-around">
       <Link
         to="/"
         className="flex items-center text-lg font-bold cursor-pointer"
@@ -11,7 +14,18 @@ const Header = () => {
         <MdStoreMallDirectory className="text-2xl" />
         <span>E-Loja</span>
       </Link>
-      <div>
+      <div className="flex gap-x-5">
+        {theme === "dark" ? (
+          <BsFillSunFill
+            className="text-2xl cursor-pointer"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <BsFillMoonStarsFill
+            className="text-2xl cursor-pointer"
+            onClick={() => setTheme("dark")}
+          />
+        )}
         <MdLocalGroceryStore className="text-2xl" />
       </div>
     </header>
