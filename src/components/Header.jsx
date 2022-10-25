cb86a784138a26f2478bc67d05594f8e06e23560
Jean <jeanncavalcante@gmail.com>
2022-10-25 00:26:07 -0300

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { MdLocalGroceryStore, MdStoreMallDirectory } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="flex bg-gray-300 py-[20px] px-3 justify-between">
+      <Link
+        to="/"
+        className="flex items-center text-lg font-bold cursor-pointer"
+      >
+        <MdStoreMallDirectory className="text-2xl" />
+        <span>E-Loja</span>
+      </Link>
+      <div>
+        <MdLocalGroceryStore className="text-2xl" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Info from "./pages/Info";
 
 const RoutesApp = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Info" element={<Info />} />
