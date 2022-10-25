dfe3c45f13b1ea5dc5ee190199b6205057506acf
Jean <jeanncavalcante@gmail.com>
2022-10-25 11:54:10 -0300

    add useContextTheme

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,12 +1,24 @@
-import { useState, createContext, Children } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
-export const ThemeContextProvider = () => {
-  const [theme, setTheme] = useState("light");
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("@theme") !== "light" ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    const removeClass = theme === "light" ? "dark" : "light";
+    root.classList.add(theme);
+    root.classList.remove(removeClass);
+    localStorage.setItem("@theme", theme);
+  }, [theme]);
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      {Children}
+      {children}
     </ThemeContext.Provider>
   );
 };
