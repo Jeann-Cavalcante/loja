403b6f5182f703fd6e34a5de9f4ccad6508ff916
Jean <jeanncavalcante@gmail.com>
2022-10-24 23:59:18 -0300

    add routas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Button from "@mui/material/Button";
+import RoutesApp from "./routes";
 
 function App() {
   return (
     <div className="App h-screen bg-gray-200">
-      <h1>Hello loja</h1>
-      <Button variant="contained">Hello World 123</Button>
+      <RoutesApp />
     </div>
   );
 }
