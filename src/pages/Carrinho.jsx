481fc8882a4405bbf33be29f9499579d751cd470
Jean <jeanncavalcante@gmail.com>
2023-04-18 14:06:25 -0300

    titulo do carrinho

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -3,7 +3,7 @@ import ListProduct from "../components/ListProduct";
 const Carrinho = () => {
   return (
     <div className="pt-[100px] flex flex-col p-4 w-full max-w-[960px] mx-auto">
-      <h1 className="text-center text-2xl font-black mb-10">Pagina do carinho</h1>
+      <h1 className="text-center text-2xl font-black mb-10">Carinho</h1>
 
       <ListProduct />
     </div>
